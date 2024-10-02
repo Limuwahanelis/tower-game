e060d947cc8faea8aee978d0487a551fdd60b242
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-10-02 16:29:01 +0200

    Move harpoon to spearte gameobject

--- Tower/Assets/Scripts/Player/PlayerSpearController.cs
+++ Tower/Assets/Scripts/Player/PlayerSpearController.cs
@@ -6,78 +6,38 @@ using UnityEngine;
 public class PlayerSpearController : MonoBehaviour
 {
     public Action OnSpearReturned;
-    public bool CanAttack => !_isThrown;
-    [SerializeField] Transform _spearHolder;
+    public bool CanAttack => !_playerHarpoon.IsThrown;
+   
     [SerializeField] Transform _player;
     [SerializeField] float _maxThrowRange = 3.8f;
     [SerializeField] float _throwSpeed;
     [SerializeField] PlayerSpearHarpoon _playerHarpoon;
-    [SerializeField] SpearChain _spearChain;
     [SerializeField] Animator _animator;
-    private bool _isThrown = false;
-    private Vector3 _throwDirection;
-    private bool _isComingBack = false;
-    private bool _hasHitEnemy = false;
-    private Vector3 _chainOffset;
+
     private void Start()
     {
-        _chainOffset = _spearChain.transform.position - transform.position;
+       
     }
     private void Update()
     {
-        if (_isThrown)
-        {
-            if (!_isComingBack)
-            {
-                if (Vector2.Distance(_spearHolder.position, transform.position) < 3.8f)
-                {
-                    transform.position += _throwDirection * Time.deltaTime * _throwSpeed;
-                    //_spearChain.transform.position = transform.position + _chainOffset;
-                   // _spearChain.CheckSegmentsForward();
-                }
-                else 
-                {
-                    _isComingBack = true;
-                }
-                if (_hasHitEnemy)
-                {
-                    _isComingBack = true;
-                }
-            }
-            else
-            {
-                if (Vector2.Distance(_spearHolder.position, transform.position) > 0.1f)
-                {
-                    Vector3 dir = (_spearHolder.position - transform.position).normalized;
-                    transform.position += dir * Time.deltaTime * _throwSpeed;
-                    transform.up = -dir;
-                    //_spearChain.transform.position = transform.position + _chainOffset;
-                    // _spearChain.CheckSegmentsback();
-                }
-                else
-                {
-                    _isComingBack = false;
-                    _isThrown = false;
-                    transform.position=_spearHolder.position;
-                    transform.SetParent(_player);
-                    transform.localRotation = Quaternion.Euler(0,0,-90);
-                    OnSpearReturned?.Invoke();
-                }
-            }
-        }
+
     }
     public void Throw(Vector3 direction)
     {
+        gameObject.SetActive(false);
+        _playerHarpoon.gameObject.SetActive(true);
+        _playerHarpoon.OnHarpoonReturned += OnHarpoonReturn;
+        _playerHarpoon.Throw(direction);
         _animator.enabled = false;
-        _playerHarpoon.OnHit += HitEnemy;
-        _isThrown = true;
-        _throwDirection = direction;
-        transform.SetParent(null);
-    }
-    private void HitEnemy()
-    {
-        _playerHarpoon.OnHit -= HitEnemy;
-        _hasHitEnemy = true;
+
+
     }
 
+    private void OnHarpoonReturn()
+    {
+        _playerHarpoon.OnHarpoonReturned -= OnHarpoonReturn;
+        _animator.enabled = true;
+        gameObject.SetActive(true);
+        _playerHarpoon.gameObject.SetActive(false);
+    }
 }
--- Tower/Assets/Scripts/Weapons/PlayerSpearHarpoon.cs
+++ Tower/Assets/Scripts/Weapons/PlayerSpearHarpoon.cs
@@ -7,17 +7,100 @@ using UnityEngine;
 public class PlayerSpearHarpoon : MonoBehaviour
 {
     public Action OnHit;
+    public Action OnHarpoonReturned;
+    public bool IsThrown => _isThrown;
     [SerializeField] Transform _player;
+    [SerializeField] Transform _spearHolder;
+    [SerializeField] float _maxThrowRange = 3.8f;
+    [SerializeField] float _throwSpeed;
+    [SerializeField] SpearChain _spearChain;
+    IPullable _pulledObject;
+    private Vector3 _throwDirection;
+    private bool _isComingBack = false;
+    private bool _hasHitEnemy = false;
+    private Vector3 _chainOffset;
+    private bool _isThrown = false;
     private void Start()
     {
-        
+        _chainOffset = _spearChain.transform.position - transform.position;
+    }
+    private void Update()
+    {
+        if (_isThrown)
+        {
+            if (!_isComingBack)
+            {
+                if (Vector2.Distance(_spearHolder.position, transform.position) < 3.8f)
+                {
+                    transform.position += _throwDirection * Time.deltaTime * _throwSpeed;
+                    //_spearChain.transform.position = transform.position + _chainOffset;
+                    // _spearChain.CheckSegmentsForward();
+                }
+                else
+                {
+                    _isComingBack = true;
+                }
+                if (_hasHitEnemy)
+                {
+                    _isComingBack = true;
+                }
+            }
+            else
+            {
+                if (Vector2.Distance(_spearHolder.position, transform.position) > 0.1f)
+                {
+                    Vector3 dir = (_spearHolder.position - transform.position).normalized;
+                    transform.position += dir * Time.deltaTime * _throwSpeed;
+                    transform.up = -dir;
+                    //_spearChain.transform.position = transform.position + _chainOffset;
+                    // _spearChain.CheckSegmentsback();
+                }
+                else
+                {
+                    _isComingBack = false;
+                    _isThrown = false;
+                    transform.position = _spearHolder.position;
+                    transform.SetParent(_player);
+                    transform.localRotation = Quaternion.Euler(0, 0, -90);
+                    StopPull();
+                    _hasHitEnemy = false;
+                    OnHarpoonReturned?.Invoke();
+                }
+            }
+        }
+    }
+    public void Throw(Vector3 throwDirection)
+    {
+        _throwDirection = throwDirection;
+        _isThrown = true;
+        transform.SetParent(null);
+
+    }
+    public void StopPull()
+    {
+        if (_pulledObject == null) return;
+        _pulledObject.EndPull();
+        _pulledObject = null;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         IPullable pull = collision.GetComponent<IPullable>();
         if (pull!=null)
         {
-            pull.StartPull(transform);
+            _pulledObject = pull;
+            _pulledObject.StartPull(transform);
+            _hasHitEnemy = true;
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (_pulledObject != null) return;
+        IPullable pull = collision.GetComponent<IPullable>();
+        if (pull != null)
+        {
+            _pulledObject = pull;
+            _pulledObject.StartPull(transform);
+            _hasHitEnemy = true;
         }
     }
 }
