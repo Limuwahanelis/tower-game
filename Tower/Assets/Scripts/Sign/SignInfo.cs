cee7c060bd1bae76d8322f4b86270fda1a7afe2a
Limuwahanelis <37908839+Limuwahanelis@users.noreply.github.com>
2024-10-07 13:18:23 +0200

    Add spikes

--- Tower/Assets/Scripts/Sign/SignInfo.cs
+++ Tower/Assets/Scripts/Sign/SignInfo.cs
@@ -1,18 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class SignInfo : MonoBehaviour
 {
+    [SerializeField] TMP_Text _text;
+    [SerializeField] float _textShowSpeed;
+    [SerializeField] float _textFadeSpeed;
+    [SerializeField] ObjectDetection _detection;
+    private float _textAlpha;
+    private float _time;
+    private Coroutine _cor;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _detection.OnObjectDetectedUnity.AddListener(StartShowtextCor);
+        _detection.OnObjectLeftdUnity.AddListener(StartHidetextCor);
     }
-
-    // Update is called once per frame
-    void Update()
+    private void StartShowtextCor()
     {
-        
+        if(_cor != null ) 
+        {
+            StopCoroutine( _cor );
+        }
+        _cor = StartCoroutine(ShowText());
+    }
+    private void StartHidetextCor()
+    {
+        if (_cor != null)
+        {
+            StopCoroutine(_cor);
+        }
+        _cor = StartCoroutine(HideText());
+    }
+    private IEnumerator ShowText()
+    {
+        while(_textAlpha<1)
+        {
+            _time += Time.deltaTime * _textShowSpeed;
+            _time = Mathf.Clamp(_time, 0, 1);
+            _textAlpha = Mathf.Lerp(0, 1, _time);
+            _text.alpha = _textAlpha;
+            yield return null;
+        }
+    }
+    private IEnumerator HideText()
+    {
+        while (_textAlpha > 0)
+        {
+            _time -= Time.deltaTime * _textFadeSpeed;
+            _time = Mathf.Clamp(_time, 0, 1);
+            _textAlpha = Mathf.Lerp(0, 1, _time);
+            _text.alpha = _textAlpha;
+            yield return null;
+        }
+    }
+    private void OnDestroy()
+    {
+        _detection.OnObjectDetectedUnity.RemoveListener(StartShowtextCor);
+        _detection.OnObjectLeftdUnity.RemoveListener(StartHidetextCor);
     }
 }
